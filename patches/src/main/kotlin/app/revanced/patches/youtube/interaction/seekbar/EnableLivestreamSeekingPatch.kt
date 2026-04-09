bcf478d411855ac2d66e55cdc9d343be0e9dcbf8
oSumAtrIX <johan.melkonyan1@web.de>
2026-04-09 18:49:27 +0200

    Add warning for livestream seeking spoof video streams requirement and add dependency
    Co-authored-by: FlaminSarge <flaminsarge@users.noreply.github.com>

--- patches/src/main/kotlin/app/revanced/patches/youtube/interaction/seekbar/EnableLivestreamSeekingPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/interaction/seekbar/EnableLivestreamSeekingPatch.kt
@@ -8,6 +8,7 @@ import app.revanced.patches.shared.misc.settings.preference.SwitchPreference
 import app.revanced.patches.youtube.misc.extension.sharedExtensionPatch
 import app.revanced.patches.youtube.misc.settings.PreferenceScreen
 import app.revanced.patches.youtube.misc.settings.settingsPatch
+import app.revanced.patches.youtube.misc.spoof.spoofVideoStreamsPatch
 import app.revanced.util.addInstructionsAtControlFlowLabel
 import app.revanced.util.findInstructionIndicesReversedOrThrow
 import com.android.tools.smali.dexlib2.Opcode
@@ -23,6 +24,7 @@ val enableLivestreamSeekingPatch = bytecodePatch(
     dependsOn(
         sharedExtensionPatch,
         settingsPatch,
+        spoofVideoStreamsPatch,
         addResourcesPatch,
     )
 
