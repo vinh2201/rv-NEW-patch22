3c08f5df5b823ff224f719747982a15d9a7d335b
Mostafa Nazari <nazarimostafa2006@gmail.com>
2026-04-21 18:02:24 +0200

    Update Twitch ad patches: v29.0.3 R8-resistant fingerprints, unconditional block, no settings dep

--- patches/src/main/kotlin/app/revanced/patches/twitch/ad/embedded/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/twitch/ad/embedded/Fingerprints.kt
@@ -1,11 +1,10 @@
 package app.revanced.patches.twitch.ad.embedded
 
-import app.revanced.patcher.definingClass
-import app.revanced.patcher.gettingFirstMethodDeclaratively
-import app.revanced.patcher.name
+import app.revanced.patcher.gettingFirstMethodDeclarativelyOrNull
+import app.revanced.patcher.strings
 import app.revanced.patcher.patch.BytecodePatchContext
 
-internal val BytecodePatchContext.createsUsherClientMethod by gettingFirstMethodDeclaratively {
-    name("buildOkHttpClient")
-    definingClass("Ltv/twitch/android/network/OkHttpClientFactory;")
+
+internal val BytecodePatchContext.usherUrlBuilderMethod by gettingFirstMethodDeclarativelyOrNull {
+    strings("usher.ttvnw.net", "force_preroll", "play_session_id")
 }
