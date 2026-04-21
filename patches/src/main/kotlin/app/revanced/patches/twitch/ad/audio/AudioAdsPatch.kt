6336cacb10a3bff73b6911fa97329971eda6551f
Mostafa Nazari <nazarimostafa2006@gmail.com>
2026-04-21 18:17:44 +0200

    Update Twitch ad patches: v29.0.3 R8-resistant fingerprints, unconditional block, no settings dep

--- patches/src/main/kotlin/app/revanced/patches/twitch/ad/audio/AudioAdsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/twitch/ad/audio/AudioAdsPatch.kt
@@ -1,46 +1,17 @@
 package app.revanced.patches.twitch.ad.audio
 
-import app.revanced.patcher.extensions.ExternalLabel
-import app.revanced.patcher.extensions.addInstructionsWithLabels
-import app.revanced.patcher.extensions.getInstruction
+import app.revanced.patcher.extensions.addInstructions
 import app.revanced.patcher.patch.bytecodePatch
-import app.revanced.patches.all.misc.resources.addResources
-import app.revanced.patches.all.misc.resources.addResourcesPatch
-import app.revanced.patches.shared.misc.settings.preference.SwitchPreference
-import app.revanced.patches.twitch.misc.extension.sharedExtensionPatch
-import app.revanced.patches.twitch.misc.settings.PreferenceScreen
-import app.revanced.patches.twitch.misc.settings.settingsPatch
 
 @Suppress("unused")
 val blockAudioAdsPatch = bytecodePatch(
     name = "Block audio ads",
-    description = "Blocks audio ads in streams and VODs.",
+    description = "Blocks audio ads in streams and VODs (best-effort on v29).",
 ) {
-    dependsOn(
-        sharedExtensionPatch,
-        settingsPatch,
-        addResourcesPatch,
-    )
-
-    compatibleWith("tv.twitch.android.app"("16.9.1", "25.3.0"))
+    compatibleWith("tv.twitch.android.app"("16.9.1", "25.3.0", "29.0.3"))
 
     apply {
-        addResources("twitch", "ad.audio.audioAdsPatch")
-
-        PreferenceScreen.ADS.CLIENT_SIDE.addPreferences(
-            SwitchPreference("revanced_block_audio_ads"),
-        )
-
-        // Block playAds call
-        audioAdsPresenterPlayMethod.addInstructionsWithLabels(
-            0,
-            """
-                    invoke-static { }, Lapp/revanced/extension/twitch/patches/AudioAdsPatch;->shouldBlockAudioAds()Z
-                    move-result v0
-                    if-eqz v0, :show_audio_ads
-                    return-void
-                """,
-            ExternalLabel("show_audio_ads", audioAdsPresenterPlayMethod.getInstruction(0)),
-        )
+        audioAdSdaMetadataMethod?.addInstructions(0, "return-void")
+        audioAdsPlayMethod?.addInstructions(0, "return-void")
     }
 }
