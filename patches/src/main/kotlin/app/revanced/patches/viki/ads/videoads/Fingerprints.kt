6035f20a73a61c713b10f3210a8c2a0901f7ff10
gajgaje2000 <gajgaje2000@gmail.com>
2026-05-28 04:11:33 -0700

    Viki patch updated with getReference()

--- patches/src/main/kotlin/app/revanced/patches/viki/ads/videoads/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/viki/ads/videoads/Fingerprints.kt
@@ -4,8 +4,8 @@ import app.revanced.patcher.anyInstruction
 import app.revanced.patcher.gettingFirstMethod
 import app.revanced.patcher.implementation
 import app.revanced.patcher.patch.BytecodePatchContext
+import app.revanced.util.getReference
 import com.android.tools.smali.dexlib2.iface.Method
-import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
 import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 
 private const val MEDIA_RESOURCE_CLASS = "Lcom/viki/library/beans/MediaResource;"
@@ -18,7 +18,8 @@ internal val BytecodePatchContext.shouldLoadVideoAdsMethod by gettingFirstMethod
 
 private fun Method.invokesBooleanMethodWith(parameterType: String) = implementation {
     anyInstruction {
-        val reference = (this as? ReferenceInstruction)?.reference as? MethodReference
+        @Suppress("DEPRECATION")
+        val reference: MethodReference = getReference()
             ?: return@anyInstruction false
 
         reference.returnType == "Z" &&
