4bc53b7fef29c98f5ca63dab837da138d8ec6aef
gajgaje2000 <gajgaje2000@gmail.com>
2026-05-30 02:57:13 -0700

    Change viki patch fingerprint with gettingFirstMethodDeclaratively

--- patches/src/main/kotlin/app/revanced/patches/viki/ads/videoads/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/viki/ads/videoads/Fingerprints.kt
@@ -1,28 +1,21 @@
 package app.revanced.patches.viki.ads.videoads
 
-import app.revanced.patcher.anyInstruction
-import app.revanced.patcher.gettingFirstMethod
-import app.revanced.patcher.implementation
+import app.revanced.patcher.gettingFirstMethodDeclaratively
+import app.revanced.patcher.instructions
+import app.revanced.patcher.method
+import app.revanced.patcher.parameterTypes
 import app.revanced.patcher.patch.BytecodePatchContext
-import app.revanced.util.getReference
-import com.android.tools.smali.dexlib2.iface.Method
-import com.android.tools.smali.dexlib2.iface.reference.MethodReference
+import app.revanced.patcher.returnType
 
 private const val MEDIA_RESOURCE_CLASS = "Lcom/viki/library/beans/MediaResource;"
 
-internal val BytecodePatchContext.shouldLoadVideoAdsMethod by gettingFirstMethod("mediaResource") {
-    returnType == "Z" &&
-        parameterTypes.isEmpty() &&
-        invokesBooleanMethodWith(MEDIA_RESOURCE_CLASS)
-}
-
-private fun Method.invokesBooleanMethodWith(parameterType: String) = implementation {
-    anyInstruction {
-        @Suppress("DEPRECATION")
-        val reference: MethodReference = getReference()
-            ?: return@anyInstruction false
-
-        reference.returnType == "Z" &&
-            reference.parameterTypes.singleOrNull()?.toString() == parameterType
-    }
+internal val BytecodePatchContext.shouldLoadVideoAdsMethod by gettingFirstMethodDeclaratively("mediaResource") {
+    returnType("Z")
+    parameterTypes()
+    instructions(
+        method {
+            returnType == "Z" &&
+                parameterTypes.singleOrNull() == MEDIA_RESOURCE_CLASS
+        }
+    )
 }
