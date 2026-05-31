2cc673a00a7420c6c1664444957ab88b1d99f84d
Ohan Melkonyan <johan.melkonyan1@web.de>
2026-05-31 19:32:50 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- patches/src/main/kotlin/app/revanced/patches/viki/ads/videoads/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/viki/ads/videoads/Fingerprints.kt
@@ -7,15 +7,13 @@ import app.revanced.patcher.parameterTypes
 import app.revanced.patcher.patch.BytecodePatchContext
 import app.revanced.patcher.returnType
 
-private const val MEDIA_RESOURCE_CLASS = "Lcom/viki/library/beans/MediaResource;"
-
 internal val BytecodePatchContext.shouldLoadVideoAdsMethod by gettingFirstMethodDeclaratively("mediaResource") {
     returnType("Z")
     parameterTypes()
     instructions(
         method {
             returnType == "Z" &&
-                parameterTypes.singleOrNull() == MEDIA_RESOURCE_CLASS
+                parameterTypes.singleOrNull() == "Lcom/viki/library/beans/MediaResource;"
         }
     )
 }
