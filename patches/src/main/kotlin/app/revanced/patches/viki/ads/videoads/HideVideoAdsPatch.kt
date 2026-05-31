ac1bace1a85c00ed7c2021d5711ee145295f9ff5
Ohan Melkonyan <johan.melkonyan1@web.de>
2026-05-31 19:33:56 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- patches/src/main/kotlin/app/revanced/patches/viki/ads/videoads/HideVideoAdsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/viki/ads/videoads/HideVideoAdsPatch.kt
@@ -4,7 +4,9 @@ import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.util.returnEarly
 
 @Suppress("unused")
-val hideVideoAdsPatch = bytecodePatch("Hide video ads") {
+val hideVideoAdsPatch = bytecodePatch(
+    name = "Hide video ads"
+) {
     compatibleWith("com.viki.android")
 
     apply {
