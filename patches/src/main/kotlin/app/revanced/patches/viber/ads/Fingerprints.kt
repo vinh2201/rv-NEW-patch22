cf1a79642ab14d78046166a577834ec59796039c
Nguyễn Tấn Vĩnh <potter.harry0909@yahoo.com>
2026-08-27 18:08:23 +0700

    test

--- patches/src/main/kotlin/app/revanced/patches/viber/ads/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/viber/ads/Fingerprints.kt
@@ -7,10 +7,10 @@ import app.revanced.patcher.parameterTypes
 import app.revanced.patcher.patch.BytecodePatchContext
 import app.revanced.patcher.returnType
 
-// Thêm matcher này vào file fingerprints.kt
-internal val BytecodePatchContext.isAdsFreeMethodMatch by firstMethodDeclaratively {
-    definingClass("Lcom/viber/voip/feature/viberplus/presentation/settings/ViberPlusSettingsState;")
-    name("isAdsFree")
-    returnType("Z") // Z tương đương với kiểu Boolean
-    parameterTypes() // Không có tham số (emptyList)
-}
+internal val BytecodePatchContext.isAdsFreeMethodMatch
+    get() = firstMethodDeclaratively {
+        definingClass("Lcom/viber/voip/feature/viberplus/presentation/settings/ViberPlusSettingsState;")
+        name("isAdsFree")
+        returnType("Z")
+        parameterTypes()
+    }
