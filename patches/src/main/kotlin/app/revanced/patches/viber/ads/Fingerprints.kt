184f7fa91d89acffe5af11b692a65b48e48f9db2
Nguyễn Tấn Vĩnh <potter.harry0909@yahoo.com>
2026-09-02 09:28:23 +0700

    test

--- patches/src/main/kotlin/app/revanced/patches/viber/ads/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/viber/ads/Fingerprints.kt
@@ -9,7 +9,7 @@ import com.android.tools.smali.dexlib2.Opcode
 internal val BytecodePatchContext.findVPlusMainMatch by composingFirstMethod {
     // Tìm trực tiếp chuỗi định danh "vPlus_Main" trong khối khởi tạo cờ
     instructions(
+        Opcode.NEW_INSTANCE(),
         "viber_plus_debug_ads_free_flag"(),
-        Opcode.CONST_STRING(),
     )
 }
