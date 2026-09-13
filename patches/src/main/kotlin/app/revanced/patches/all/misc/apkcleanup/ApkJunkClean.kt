392e8e3d63a78441d0ba85f834c3618b18cee02d
Nguyễn Tấn Vĩnh <63141930+vinh2201@users.noreply.github.com>
2026-09-13 13:57:01 +0700

    test

--- patches/src/main/kotlin/app/revanced/patches/all/misc/apkcleanup/ApkJunkClean.kt
+++ patches/src/main/kotlin/app/revanced/patches/all/misc/apkcleanup/ApkJunkClean.kt
@@ -174,6 +174,8 @@ private val EXACT_ROOT_JUNK = listOf(
 
     // === BỔ SUNG CÁC MỤC BỊ SÓT SO VỚI REGEX LIST ===
     "ion-java.properties",
+    "feature-delivery.properties",
+    "feature-delivery-base.properties"
     "facebook_trackers.xml",
     "google_trackers.xml",
     "firebase_trackers.xml",
@@ -201,7 +203,8 @@ private val EXACT_ROOT_JUNK = listOf(
     "debug.keystore", 
     "version.properties", 
     "integrity.properties", 
-    "androidannotations-api.properties",
+    "androidannotations-api.properties", 
+    "jetty-dir.css",
     "tv-ads.properties"
 )
 
